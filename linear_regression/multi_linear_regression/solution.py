ea301a72e05cb4ff8cafdf65d65adc5230cd0934
thomas.wangsa@finaccel.co <thomas.wangsa@finaccel.co>
2020-02-10 16:19:49 +0700

    first commit

---
+++ linear_regression/multi_linear_regression/solution.py
@@ -0,0 +1,40 @@
+# TODO: Add import statements
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+# Assign the data to predictor and outcome variables
+# TODO: Load the data
+train_data = pd.read_csv('data.csv')
+X = train_data['Var_X'].values.reshape(-1, 1)
+y = train_data['Var_Y'].values
+
+# Create polynomial features
+# TODO: Create a PolynomialFeatures object, then fit and transform the
+# predictor feature
+poly_feat = PolynomialFeatures(degree = 4)
+X_poly = poly_feat.fit_transform(X)
+
+# Make and fit the polynomial regression model
+# TODO: Create a LinearRegression object and fit it to the polynomial predictor
+# features
+poly_model = LinearRegression(fit_intercept = False).fit(X_poly, y)from sklearn.linear_model import LinearRegression
+from sklearn.datasets import load_boston
+
+# Load the data from the boston house-prices dataset 
+boston_data = load_boston()
+x = boston_data['data']
+y = boston_data['target']
+
+# Make and fit the linear regression model
+# TODO: Fit the model and Assign it to the model variable
+model = LinearRegression()
+model.fit(x, y)
+
+# Make a prediction using the model
+sample_house = [[2.29690000e-01, 0.00000000e+00, 1.05900000e+01, 0.00000000e+00, 4.89000000e-01,
+                6.32600000e+00, 5.25000000e+01, 4.35490000e+00, 4.00000000e+00, 2.77000000e+02,
+                1.86000000e+01, 3.94870000e+02, 1.09700000e+01]]
+# TODO: Predict housing price for the sample_house
+prediction = model.predict(sample_house)
